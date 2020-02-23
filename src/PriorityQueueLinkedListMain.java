c22cd52103bf94319b298e9e4fde56a8349f1d95
neer2808 <uic.neerajkhanna@gmail.com>
2020-02-23 14:38:31 +0530

    init comit

--- src/PriorityQueueLinkedListMain.java
+++ src/PriorityQueueLinkedListMain.java
@@ -1,5 +1,8 @@
+
+
 public class PriorityQueueLinkedListMain {
   public static void main(String[] args) {
+  // Creation of the node class object
     Node n1= new Node(15,4);
     Node n2 = new Node(10,3);
     Node n3 = new Node(20,3);
